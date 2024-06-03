1330e471d595b4cc97df6535bf9efd760d786d68
Angel David <angeldavidstudios@outlook.com>
2024-06-03 07:10:14 -0500

    Inicio del Examen

---
+++ ExamenP1_DavidRueda/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenP1_DavidRueda;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ ExamenP1_DavidRueda/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExamenP1_DavidRueda;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
