bd29622b65149b2173418f71965bf4ff5d908ccf
Angel David <angeldavidstudios@outlook.com>
2024-06-03 07:14:15 -0500

    Creacion del MVVM y el Model

---
+++ ExamenP1_DavidRueda/MVVM/Models/RecargaValues.cs
@@ -0,0 +1,24 @@
+namespace ExamenP1_DavidRueda.MVVM.Models;
+
+public class RecargaValues
+{
+    public string Recarga { get; set; }
+    
+    public bool Option1
+    {
+        get => Recarga == "3";
+        set { if (value) Recarga = "3"; }
+    }
+    
+    public bool Option2
+    {
+        get => Recarga == "5";
+        set { if (value) Recarga = "5"; }
+    }
+
+    public bool Option3
+    {
+        get => Recarga == "10";
+        set { if (value) Recarga = "10"; }
+    }
+}
